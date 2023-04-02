bfc12203b068ce4a0a2199ce3e0b6e9f8b349a10
yijq <1325260417@qq.com>
2023-04-02 23:10:50 +0800

    first commit

---
+++ src/main/java/com/yjq/Main.java
@@ -0,0 +1,11 @@
+package com.yjq;
+/**
+ * @description:
+ * @author: Created by yijq
+ * @date: 2023/4/2 23:05
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
